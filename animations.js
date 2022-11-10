7b8790d7136c343e29d47c77f3227701e05adab3
shree <sammmmmu123@gmail.com>
2022-11-10 19:34:29 +0530

    backup

--- animations.js
+++ animations.js
@@ -1,5 +1,8 @@
 export const dropdown = () => {
   document.getElementById('reveal').addEventListener('click', (e) => {
-    document.querySelector('#hidden-buts').classList.toggle('show')
+    document.querySelectorAll('#roundbuts .but').forEach((e) => {
+      e.classList.toggle('flatten')
+    })
+    document.querySelector('.hidden-buts').classList.toggle('show')
   })
 }
